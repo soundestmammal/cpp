0194828fe3261d9ebe265056fe01ea98c78f20ea
Robert Checco <robert.checco@gmail.com>
2018-12-31 22:39:37 -0500

    Input and output for puppy age

---
+++ puppy/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age {};
+    cout << "How many years old is your puppy?" << endl;
+    cin >> age;
+    cout << "Wow your puppy is " << age << " years old!" << endl;
+    return 0;
+}
