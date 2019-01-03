7f9f7db0ea98563dd105d9f70f2946c93488da14
Robert Checco <robert.checco@gmail.com>
2019-01-02 23:53:32 -0500

    Array and Primitives in C++

---
+++ array/main.cpp
@@ -0,0 +1,4 @@
+// Why use an array?
+
+// Keep track of 10,000 test scores.
+// Collection of elements and give it a specific name
---
+++ primitive/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //char is one byte. At least 8 bits.
+
+    //int are whole numbers. signed and unsigned
+    // age would be an unsigned int
+    // distance between planets unsigned long long int
+
+    // floating point types
+    // represent real numbers
+    // float, double, long double
+
+    //boolean data type
+    // 0 is false
+    // any non zero is true
+    // true or false (lower case in c++)
+    
+
+
+}
