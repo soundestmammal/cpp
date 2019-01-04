80520849473ae822e23a4a3d833456259cd4036d
Robert Checco <robert.checco@gmail.com>
2019-01-04 12:17:13 -0500

    Volume of a Cylinder Function

--- Section11/FunctionDefinition/main.cpp
+++ Section11/FunctionDefinition/main.cpp
@@ -12,9 +12,21 @@ void area_circle() {
     cin >> radius;
     cout << "\nThe area of the circle with radius " << radius << " is " << calc_area_circle(radius) << endl;
 }
+double calc_volume_cylinder(double radius, double height) {
+    return pi * radius * radius * height;
+}
+void volume_cylinder() {
+    double radius {};
+    double height {};
+    cout << "\nEnter the radius of the cylinder: ";
+    cin >> radius;
+    cout << "\nEnter the height of the cylinder: ";
+    cin >> height;
+    cout << "\nThe volume of cylinder with radius " << radius << " and height " << height << " is " << calc_volume_cylinder(radius, height) << endl;
+}
 int main()
 {
     area_circle();
-    //volume_cylinder();
+    volume_cylinder();
     return 0;
 }
