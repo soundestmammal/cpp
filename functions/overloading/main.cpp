17fc46769c0a08d6f7d7628aae8b0e8168057982
Robert Checco <robert.checco@gmail.com>
2019-01-08 21:47:11 -0500

    Practice overloading functions

---
+++ functions/overloading/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+    void print(int);
+    void print(double);
+    void print(string);
+    void print(string, string);
+    void print(vector<string>);
+
+    void print(int num){
+        cout << "Printing int:" << num << endl;
+    };
+    void print(double num){
+        cout << "Print double:" << num << endl;
+    };
+    void print(string s){
+        cout << "Print string:" << s << endl;
+    };
+    void print(string s, string t){
+        cout << "Printing two strings " << s << " and " << t << endl;
+    }
+
+int main()
+{
+    print(100);
+    return 0;
+}
