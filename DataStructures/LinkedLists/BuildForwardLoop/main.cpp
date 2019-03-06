766bc07417b3a326bd948ca2b0a6ddea0a268dec
Robert Checco <robert.checco@gmail.com>
2019-03-06 18:45:40 -0500

    Successfully built a linked list with multiple nodes

---
+++ DataStructures/LinkedLists/BuildForwardLoop/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+struct nodeType
+{
+    int info;
+    nodeType *link;
+};
+
+nodeType buildListForward()
+    {
+        nodeType *first, *last, *newNode, *current;
+        int num;
+
+        cout << "Enter a list of numbers to make a linked list with the last number being -999" << endl;
+        cin >> num;
+        first = NULL;
+        
+        while(num != -999)
+        {
+            newNode = new nodeType;
+            newNode->info = num;
+            newNode->link = NULL;
+            
+            if (first == NULL) {
+                first = newNode;
+                last = newNode;
+            }
+            else {
+                last->link = newNode;
+                last = newNode;
+            }
+            cin >> num;
+        }
+        current = first;
+        while (current != NULL)
+        {
+            cout << current->info << " ";
+            current = current->link;
+        }
+        
+    };
+
+int main()
+{
+    buildListForward();
+    return 0;
+}
