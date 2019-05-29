c0c50fc5c2e37875f3eb17598ac0d0f6003318b0
Robert Checco <robert.checco@gmail.com>
2019-05-29 10:17:29 -0400

    In class example 1

---
+++ cs120/Week1/Class2/Example1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "What number is your favorite?\n";
+    cin >> num;
+
+    cout << num << " is a good choice." << endl;
+
+    return 0;
+}
