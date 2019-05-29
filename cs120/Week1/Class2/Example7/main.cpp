6d60bd2059095d8c71894703080a2adcd009474f
Robert Checco <robert.checco@gmail.com>
2019-05-29 15:52:20 -0400

    Examples 7 & 8

---
+++ cs120/Week1/Class2/Example7/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    // Characters use single quotes
+    char letter = 'a';
+    char another_letter = 'b';
+    // Shifts the letter a by one
+    char moreLetters = letter + 1;
+
+    char evenMoreLetters = letter + 'm';
+
+    cout << moreLetters << endl;
+    cout << (char)65 << endl;
+    
+    return 0;
+}
---
+++ cs120/Week1/Class2/Example8/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    
+    // Using Strings
+
+    string sentence;
+    cout << "Give me a string\n";
+    cin >> sentence;
+
+    cout << "Here is your sentence: " << sentence << endl;
+    return 0;
+}
