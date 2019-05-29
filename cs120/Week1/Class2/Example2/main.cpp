8a13a301df5fb93a8896ba860e5b6731b03f5796
Robert Checco <robert.checco@gmail.com>
2019-05-29 10:26:22 -0400

    In class Example 2 (order of operation, assignment)

---
+++ cs120/Week1/Class2/Example2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    double num1, num2, num3;
+    cout << "What is the first number ";
+    cin >> num1;
+    cout << "What is the second number? ";
+    cin >> num2;
+    
+    num3 = (num1 + num2) / 2;
+    cout << "The average is " << num3 << endl;
+
+    return 0;
+}
