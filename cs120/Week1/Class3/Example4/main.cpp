26bd7127dbbdc9371572deda029b5c01f40043d9
Robert Checco <robert.checco@gmail.com>
2019-05-30 11:03:59 -0400

    Shift the range of random numbers

--- cs120/Week1/Class3/Example4/main.cpp
+++ cs120/Week1/Class3/Example4/main.cpp
@@ -7,8 +7,11 @@ using namespace std;
 
 int main() {
     
+    // Numbers between 1 and 6
     srand(time(NULL));
-    cout << rand() << endl;
+    cout << (rand() % 6) + 1 << endl; // numbers between 1 and 6
+    cout << (rand() % 100) + 1 << endl; // numbers between 1 and 100
+    cout << (rand() % 51) + 50 << endl; //  numbers between 50 and 100
 
     return 0;
 }
