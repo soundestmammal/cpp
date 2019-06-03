c1f8127091d5830cce23ba576f7bd5f15fade3ab
Robert Checco <robert.checco@gmail.com>
2019-06-03 15:00:01 -0400

    Monday Class

---
+++ cs120/Week2/Monday/Example3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <string>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    char letter;
+
+    cout << "What is your favorite letter\n";
+    cin >> letter;
+
+    char shiftedLetter = letter + 1;
+    cout << shiftedLetter << endl;
+
+    return 0;
+}
