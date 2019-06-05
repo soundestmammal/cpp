2a4fbb3e0cf1a39c957cb14120ecd479f47f7da0
Robert Checco <robert.checco@gmail.com>
2019-06-05 11:01:11 -0400

    Amusement Park logic

--- cs120/Week2/Wednesday/Example2/main.cpp
+++ cs120/Week2/Wednesday/Example2/main.cpp
@@ -13,13 +13,49 @@ int main() {
     int groupSize {};
     string ageType {};
     double basePrice, total;
+    double extraServiceCharge {1.1};
+    double ageTypePrice {};
 
     cout << "How many people are in your group? \n";
     cin >> groupSize;
+    if (groupSize < 1 || groupSize > 50) {
+        cout << "How many people are in your group? \n";
+        cin >> groupSize;
+    }
+    
     cout << "Are you a child, adult or senior?\n";
     cin >> ageType;
+    if (ageType != "child" || ageType != "adult" || ageType != "senior") {
+        cout << "Are you a child, adult or senior?\n";
+        cin >> ageType;
+    }
+
+    if (groupSize < 10) {
+        if (ageType == "child") {
+            ageTypePrice = 10;
+        } 
+        else if (ageType == "adult") {
+            ageTypePrice = 20;
+        }
+        else if (ageType == "senior") {
+            ageTypePrice = 15;
+        }
+        basePrice = groupSize * ageTypePrice;
+    } else if (groupSize >= 10) {
+        if (ageType == "child") {
+            ageTypePrice = 10;
+        } 
+        else if (ageType == "adult") {
+            ageTypePrice = 20;
+        }
+        else if (ageType == "senior") {
+            ageTypePrice = 15;
+        }
+        basePrice = groupSize * ageTypePrice * extraServiceCharge;
+    }
+    total = basePrice * (1+TAX);
+    cout << "Your total cost is " << total << endl;
 
     
-
     return 0;
 }
