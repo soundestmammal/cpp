4dac9343905322da7fa8dcbf6489410daf8a91d3
Robert Checco <robert.checco@gmail.com>
2019-06-05 11:23:18 -0400

    Weather logic

---
+++ cs120/Week2/Wednesday/Example3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+    
+    bool warm, raining;
+
+    cout << "Is it hot today?";
+    cin >> warm;
+    cout << "Is it raining today?\n";
+    cin >> raining;
+
+    if (warm && raining) {
+        cout << "Short sleeve shirt and bring an umbrella." << endl;
+    }
+    else if (warm && !raining) {
+        cout << "Just short sleeves \n";
+    }
+    else if (!warm && raining) {
+        cout << "Long sleeves and an umbrella" << endl;
+    }
+    else if (!warm && !raining) {
+        cout << "Just long sleeves" << endl;
+    }
+    
+    return 0;
+}
