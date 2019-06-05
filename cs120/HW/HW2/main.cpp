66aa275441d164d4a9f1813257e2dee01683a245
Robert Checco <robert.checco@gmail.com>
2019-06-05 16:39:22 -0400

    pset2/q2

--- cs120/HW/HW2/main.cpp
+++ cs120/HW/HW2/main.cpp
@@ -35,6 +35,29 @@ int main() {
        cout << "Sorry, that's not a primary color." << endl;
    }
 
+   // Question 2 on Problem Set
+   /*
+   2. Create a random number between 1 and 100.  
+   If it is less than 50 output "you chose a number less than 50." 
+   If it is more than 50, output "you chose a number more than 50."
+   */
+    srand(time(NULL));
+    // Generate a Random number between 0-99 (Add one to make 1-100)
+    int randNum = rand() % 100 + 1;
+    if (randNum < 50) {
+        cout << "You chose a number less than or equal to 50" << endl;
+    } else if (randNum > 50){
+        cout << "You chose a number greater than 50" << endl;
+    } else {
+        cout << "The number you chose is 50" << endl;
+    }
+
+    // Question 3
+
+    
+
+
+
     // Question 8 on Problem Set
     double length1 {};
     double width1 {};
