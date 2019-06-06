887db5677fea554e2dcce18f81c9fa53866e2090
Robert Checco <robert.checco@gmail.com>
2019-06-06 11:44:24 -0400

    Enum example car options

---
+++ cs120/Week2/Thursday/Example4/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <iomanip>
+#include <fstream>
+#include <ctime>
+#include <time.h>
+
+using namespace std;
+
+enum Cars { McClaren = 1, Tesla = 2, Jaguar = 3, Aston_Martin = 4 };
+
+int main() {
+    int car {};
+    cout << "Which car would you like option 1, 2, 3, 4?\n";
+    cin >> car;
+
+    switch(car) {
+        case McClaren:
+            cout << "Cool" << endl;
+            break;
+        case Tesla:
+            cout << "Hold on during acceleration..." << endl;
+            break;
+        case Jaguar:
+            cout << "Everyone says this one differently" << endl;
+            break;
+        case Aston_Martin:
+            cout << "This name has two words" << endl;
+            break;
+        default:
+            cout << "I don't know that kind of car... " << endl;
+            break;
+    }
+
+    return 0;
+}
