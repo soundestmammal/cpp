6c5d092caea0b9ea7a6fb915c4526804ccae3a19
Robert Checco <robert.checco@gmail.com>
2019-06-13 11:45:53 -0400

    W3-thurs Example1

--- cs120/Week3/Thursday/Example1/main.cpp
+++ cs120/Week3/Thursday/Example1/main.cpp
@@ -5,15 +5,35 @@
 
 using namespace std;
 
+void sayHi();
+void helloToYou(string name);
+
 int main() {
-    // while loop from 2 to 10
+    string res;
+    cout << "What is your name  ";
+    cin >> res;
 
-    int num = 2;
-    while(num <= 10) {
-        cout << num++ << " ";
-    }
-
-    cout << endl;
-    
+    helloToYou(res);
     return 0;
 }
+
+void sayHi() {
+    cout << "Hello there" << endl;
+}
+
+void helloToYou(string name) {
+    cout << "Hello there " << name << endl;
+}
+
+/* 
+Four components to a function in cpp
+
+1. Function Header
+    * Return Type
+    * Function Name
+    * Inputs
+2. Body
+    * Code that will run
+3. Put Function Prototype above main
+4. Invoke the function
+ */
