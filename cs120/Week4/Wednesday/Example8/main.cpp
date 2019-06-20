7faf46e46968519c8c5e677e3326bf69f041b327
Robert Checco <robert.checco@gmail.com>
2019-06-20 14:24:11 -0400

    w4-wed-ex8

---
+++ cs120/Week4/Wednesday/Example8/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <iomanip>
+#include <fstream>
+#include <ctime>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+void Echo();
+
+int main() {
+    
+
+
+    return 0;
+}
+
+void Echo() {
+    for (int i = 0; i < 20; i++)
+    {
+        cout << i << endl;
+    }
+    
+}
