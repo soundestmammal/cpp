52766ce7a45639dd65e458352b16f61ffcbb6171
Robert Checco <robert.checco@gmail.com>
2019-06-21 01:38:20 -0400

    Function definitions and prototypes

--- learncpp/S11-Functions/FunctionDefinition/main.cpp
+++ learncpp/S11-Functions/FunctionDefinition/main.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
 
-using namespace std;
 const double pi {3.14159};
+
+void area_circle();
+double calc_area_circle(double radius);
+double calc_volume_cylinder(double radius, double height);
+void volume_cylinder();
+
+
+int main()
+{
+    area_circle();
+    volume_cylinder();
+    return 0;
+}
 
 double calc_area_circle(double radius) {
     return pi * radius * radius;
 }
 void area_circle() {
     double radius {};
-    cout << "\nEnter the radius of the circle ";
-    cin >> radius;
-    cout << "\nThe area of the circle with radius " << radius << " is " << calc_area_circle(radius) << endl;
+    std::cout << "\nEnter the radius of the circle ";
+    std::cin >> radius;
+    std::cout << "\nThe area of the circle with radius " << radius << " is " << calc_area_circle(radius) << endl;
 }
 double calc_volume_cylinder(double radius, double height) {
-    //return pi * radius * radius * height;
     return calc_area_circle(radius) * height;
 }
 void volume_cylinder() {
     double radius {};
     double height {};
-    cout << "\nEnter the radius of the cylinder: ";
-    cin >> radius;
-    cout << "\nEnter the height of the cylinder: ";
-    cin >> height;
-    cout << "\nThe volume of cylinder with radius " << radius << " and height " << height << " is " << calc_volume_cylinder(radius, height) << endl;
-}
-int main()
-{
-    area_circle();
-    volume_cylinder();
-    return 0;
+    std::cout << "\nEnter the radius of the cylinder: ";
+    std::cin >> radius;
+    std::cout << "\nEnter the height of the cylinder: ";
+    std::cin >> height;
+    std::cout << "\nThe volume of cylinder with radius " << radius << " and height " << height << " is " << calc_volume_cylinder(radius, height) << endl;
 }
