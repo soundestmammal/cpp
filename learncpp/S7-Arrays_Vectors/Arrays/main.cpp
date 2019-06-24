bbc677ef20f1a0133a71ebc2d722328bc4b06a37
Robert Checco <robert.checco@gmail.com>
2019-06-24 12:35:20 -0400

    lcpp arrays and vectors

--- learncpp/S7-Arrays_Vectors/Arrays/main.cpp
+++ learncpp/S7-Arrays_Vectors/Arrays/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-    char vowels [] {'a', 'e', 'i', 'o', 'u'};
+    char vowels [] {'a', 'e', 'i', 'o', 'u'};  // "Hey compiler, you tell me how big this array should be"
     cout << "\nThe first vowel is: " << vowels[0] << endl;
     cout << "The last vowel is: " << vowels[4] << endl;
 
@@ -13,11 +13,13 @@ int main() {
     cout << "\nThe first high temperature is: " << hi_temps[0] << endl;
 
     hi_temps[0] = 100.7; // set the first element in hi_temps to 100.7
-                            // we'll take about assignment statement in an upcoming section
     
     cout << "The first high temperature is now: " << hi_temps[0] << endl;
 
     int test_scores[5] {};
+    // int test_scores[5] {0};
+    // int test_scores[5] {100}; cout -> [100, 0, 0, 0, 0]
+    // int test_scores[5] {100, 90}; cout -> [100, 90, 0, 0, 0]
 
     cout << "\nFirst score at index 0: " << test_scores[0] << endl;
     cout << "Second score at index 1: " << test_scores[1] << endl;
