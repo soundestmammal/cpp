f97a1a4a5dd04642c039f5eb17aef9a497e9326e
Robert Checco <robert.checco@gmail.com>
2019-06-24 12:36:14 -0400

    w5-mon-ex1

---
+++ cs120/Week5/Monday/Example1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    const int SIZE = 3;
+    int list[SIZE];
+
+    list[0] = 30;
+    list[1] = -5;
+    list[2] = 10;
+
+    cout << list[2] << endl;
+
+    return 0;
+}
