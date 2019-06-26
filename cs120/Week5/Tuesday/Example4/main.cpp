7df581d8ee1cba1759d7b0fcf9b707be6664cd06
Robert Checco <robert.checco@gmail.com>
2019-06-25 23:57:52 -0400

    w5-tues-ex4

---
+++ cs120/Week5/Tuesday/Example4/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <string>
+
+// Function Proto
+std::string india1(int num);
+std::string india1(double num);
+
+int main () {
+    
+
+    return 0;
+}
