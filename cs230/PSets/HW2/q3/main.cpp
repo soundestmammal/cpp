8e4bfbddcbaf36591d409160e930a77c49ecea99
Robert Checco <robert.checco@gmail.com>
2019-07-19 17:39:26 -0400

    Copy elements and insert a new element into a new array

---
+++ cs230/PSets/HW2/q3/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+// Function Header/Prototype Things:
+void copyAndInsertArray(int a[], int size1, int b[], int size2, int position, int payload);
+
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+    const int SIZE = 10;
+    int emptyArray[11];
+    int original[SIZE];
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        original[i] = rand() % 101 + 1;
+    }
+
+    copyAndInsertArray(emptyArray, 11, original, SIZE, 5, 69);
+    
+    ofstream output;
+    output.open("new.txt");
+
+    for (int i = 0; i < 11; i++)
+    {
+        output << emptyArray[i] << endl;
+    }
+    
+    output.close();
+
+    output.open("original.txt");
+
+    for (int i = 0; i < 10; i++)
+    {
+        output << original[i] << endl;
+    }
+
+    output.close();
+    return 0;
+}
+
+// The first array and size is empty.
+// The second array and size is the array to be copied.
+void copyAndInsertArray(int a[], int size1, int b[], int size2, int position, int payload) {
+    for (int i = 0; i < size1; i++)
+    {
+        if(i == position) {
+            a[position] = payload;
+        } else if (i < position){
+            a[i] = b[i];
+        } else {
+            a[i] = b[i-1];
+        }
+    }
+}
+
