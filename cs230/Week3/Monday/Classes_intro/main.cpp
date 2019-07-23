cc6d57286dde0e27ad9a414eed7871e296397845
Robert Checco <robert.checco@gmail.com>
2019-07-23 12:05:28 -0400

    Link cpp and h files, and use in main

---
+++ cs230/Week3/Monday/Classes_intro/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#include <fstream>
+#include "Triangle.h"
+
+using namespace std;
+
+int main() {
+    Triangle t(10, 5);
+    cout << t.getHeight() << endl;
+
+    return 0;
+}
+
