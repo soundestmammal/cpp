d5af04b2f5578ffc2ee8eeb3949c7fc7c923c49e
Robert Checco <robert.checco@gmail.com>
2019-07-25 11:47:57 -0400

    Create an instance of workerbees in main - wb

---
+++ cs230/Week3/Wednesday/Example5/main.cpp
@@ -0,0 +1,15 @@
+#include "workerbees.h"
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    WorkerBees wb(3);
+    wb.showInfo();
+
+    return 0;
+}
