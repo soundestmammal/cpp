f7bc2594a44027ae104344b9785a47c861ccce31
Robert Checco <robert.checco@gmail.com>
2019-09-02 16:58:03 -0400

    Remove current pointer,  add bool isEmpty prototype

--- DataStructures/LinkedLists/LinkedList.h
+++ DataStructures/LinkedLists/LinkedList.h
@@ -6,10 +6,10 @@ private:
     int length;
     Node* head;
     Node* tail;
-    Node* current;
 public:
     LinkedList(); // Constructor
     ~LinkedList(); // Destructor
+    bool isEmpty(); // Checks if the LL is empty
     void buildItForward(); // Method to build LL with user input data
     void traverse(); // Method to traverse LL and print out data to console
     int getLength();  // Get the length of the LL
