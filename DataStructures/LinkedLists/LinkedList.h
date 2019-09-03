3cbab9ecf5a98ec2f6c40bb3747b6c91844b1c3b
Robert Checco <robert.checco@gmail.com>
2019-09-03 17:01:55 -0400

    Build, INitialize list, insertFirst methods and checklist thing

--- DataStructures/LinkedLists/LinkedList.h
+++ DataStructures/LinkedLists/LinkedList.h
@@ -10,7 +10,7 @@ public:
     LinkedList(); // Constructor
     ~LinkedList(); // Destructor
     bool isEmpty(); // Checks if the LL is empty
-    void buildItForward(); // Method to build LL with user input data
+    void build(int size); // Method to build LL with user input data
     void traverse(); // Method to traverse LL and print out data to console
     int getLength();  // Get the length of the LL
     int getFirst(); // Get data from the first item in LL
@@ -19,4 +19,27 @@ public:
     void deleteList(); // Deallocate the memory from the heap that the Linked Lists required
     Node* nthItem(int count); // Return the memory address of the third node
     void deleteItem(const Node* item); // Given an item, delete the item from the list
+    void initializeList(); // Reset the Linked List to an empty state
+    void insertFirst(int data);
 };
+
+/*
+
+[x] isEmptyList
+[x] Default Constructor
+[x] destroyList
+[x] initializeList
+[x] length
+[x] front
+[x] back
+[x] search
+[ ] insertFirst
+[ ] insertLast
+[x] deleteNode
+[ ] copyList
+[x] Destructor
+[ ] Copy Constructor
+[ ] Overloading the assignment operator
+[ ] Overloading the stream insertion operator
+
+*/
