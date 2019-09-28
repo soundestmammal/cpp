f44a0bd3f3893110e251edb825358871de79bb31
Robert Checco <robert.checco@gmail.com>
2019-09-28 18:43:54 -0400

    It works, needs to be cleaned up

--- AStarSolver 2/AStarSolver/studentsCompleteThisCode.cpp
+++ AStarSolver 2/AStarSolver/studentsCompleteThisCode.cpp
@@ -8,12 +8,13 @@
 #include "adjacencyList.h"
 
 struct compare {
-	bool operator()(const farmingdale::adjNode &a, const farmingdale::adjNode &b){
-		return a.calculatedCost > b.calculatedCost;
+	bool operator()(const farmingdale::adjLink &a, const farmingdale::adjLink &b){
+		return a.calcCost > b.calcCost;
 	}
 };
 
 farmingdale::status farmingdale::adjacencyList::computeAStar(int fromNode, int toNode, int &length, int *returnList) {
+	std::vector<int> ans;
 	// note: the constant DBL_MAX is the max value double
 	const double LARGE_DISTANCE = DBL_MAX / 2; // feel free to use as initial distance for all nodes
 	length = 0;
@@ -21,8 +22,88 @@ farmingdale::status farmingdale::adjacencyList::computeAStar(int fromNode, int t
 	// When you complete, returnList[] should contain the list of nodes in order to get from fromNode to toNode
 	// length should contain the full path length
 	// and you should return SUCCESS
-	std::priority_queue<adjNode, std::vector<adjNode>, compare> pq;
+	std::priority_queue<adjLink, std::vector<adjLink>, compare> pq;
 
+	int currentNode = fromNode;
+
+	while(currentNode != toNode) {
+		std::cout << "Currently exploring node number:	" << currentNode << std::endl;
+		adjLink *currentEdge = list[currentNode].edges;
+
+
+		while(currentEdge != NULL) {
+			//std::cout << "This is the number of the currentNode: " << currentNode << std::endl;
+			//std::cout << "This is the weight of the currentEdge" << currentEdge->weight << std::endl;
+			std::cout << "This is the connects to node" << currentEdge->connectsToNode << std::endl;
+
+
+			if(list[currentNode].distanceFromSource == 0) {
+				currentEdge->calcCost = currentEdge->weight + slDistance(currentEdge->connectsToNode, toNode);
+				list[currentEdge->connectsToNode].distanceFromSource = currentEdge->weight;
+			} else {
+				currentEdge->calcCost = list[currentNode].distanceFromSource + currentEdge->weight + slDistance(currentEdge->connectsToNode, toNode);
+				list[currentEdge->connectsToNode].distanceFromSource = currentEdge->calcCost - slDistance(currentEdge->connectsToNode, toNode);
+			}
+
+			// I need to update the distanceFromSource of the child nodes or currentEdge->connectsToNode->DISTANCE... = current-
+			list[currentEdge->connectsToNode].parent = currentNode;
+
+			std::cout << "I am currently exploring node " << currentNode << " edges. " << std::endl;
+			std::cout << currentNode << " goes to " << currentEdge->connectsToNode << std::endl;
+			std::cout << "Therefore, the parent of " << list[currentEdge->connectsToNode].parent << " should equal " << currentNode << std::endl;
+			std::cout << "About to push into the Queue" << std::endl;
+			pq.push(*currentEdge);
+
+			currentEdge = currentEdge->next;
+		}
+
+		//while(!pq.empty()) {
+			currentNode = pq.top().connectsToNode;
+			if(currentNode == toNode) {
+				// std::cout << list[currentNode].parent << std::endl;
+				ans.push_back(toNode);
+				int currentParent = toNode;
+				while(currentParent != 0) {
+					currentParent = list[currentParent].parent;
+					std::cout << "Going to push: " << currentParent;
+					std::cout << "\n";
+					ans.push_back(currentParent);
+				}
+			}
+			std::cout << "This is the min calc cost	" << pq.top().calcCost << std::endl;
+
+			pq.pop();
+		//}
+		std::cout << "\n\n\n";
+	}
+	for (int i = 0; i < ans.size(); i++)
+	{
+		std::cout << ans.at(i) << "\t";
+	}
+	
+	length = ans.size();
+	for (int i = 0; i < ans.size(); i++)
+	{
+		*(returnList + i) = ans[4-i];
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << *(returnList + i) << std::endl;
+	}
+	
+	std::cout << std::endl;
+	
+
+	
+	return SUCCESS;
+}
+
+/*
 	// Access the first node and print out the adjacent edges
 	int currentNode = fromNode;
 	while(currentNode != toNode) {
@@ -85,6 +166,5 @@ farmingdale::status farmingdale::adjacencyList::computeAStar(int fromNode, int t
 	// 	returnList = ptr;
 	// }
 	// length = 5;
-	
-	return SUCCESS;
-}
+
+	*/
