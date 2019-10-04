7992a0f737ec8a0caad48518b3d0e36284cd6ed4
Robert Checco <robert.checco@gmail.com>
2019-10-04 14:18:16 -0400

    Layout for Search Test for my Linked List

--- DataStructures/P-Sets/A8/2019LinkedList/2019LinkedList/myTests.cpp
+++ DataStructures/P-Sets/A8/2019LinkedList/2019LinkedList/myTests.cpp
@@ -91,3 +91,36 @@ bool backTest() {
 
     return false;
 };
+
+// search test
+
+/*
+    How would I test such a thing? 
+
+    Ok. I need to push a bunch of nodes
+
+    1. Randomly generate a number [0, 1000] which will node we look for
+
+    2. I will add nodes to the front
+
+    3. if ( i == randomNumber) => insert the searching string into the linked list
+
+    4. I will now have a LL with one node that is flagged for searching
+
+    5. Perform the search.
+
+    Upon Finding the NODE Perform these steps:
+        6. Count the number of traversal steps
+        7. Number of traversal steps should be the same.
+        8. The Data should be the same. 
+        9. The successor should be the same.
+*/
+
+/* 
+    Then I can test removing the node:
+
+        11. Remove the node from both data structures
+        12. Call search on the LL and List
+        13. Both the search() => 0
+            exit(1) if they are not equal.
+*/
