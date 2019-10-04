6df56fed5a7f928eccf4fb21f767419f88896e3e
Robert Checco <robert.checco@gmail.com>
2019-10-03 23:55:49 -0400

    Fix addToFront, removeBack, addToBack, removeFront

--- DataStructures/P-Sets/A8/2019LinkedList/2019LinkedList/linkedList.cpp
+++ DataStructures/P-Sets/A8/2019LinkedList/2019LinkedList/linkedList.cpp
@@ -17,68 +17,92 @@ farmingdale::LinkedList::~LinkedList() {
 
 // addToFront
 farmingdale::status farmingdale::LinkedList::addToFront(std::string addMe) {
-    // temp pointer to head
-    Node* temp = head;
-    // head points to addMe!
-    try {
-        head = new Node(addMe);
-    } catch(std::bad_alloc &ba) {
-        ba.what();
-        return FAILURE;
-    }
-        // head next points to the temp pointer
-        head->next = temp;
+    // try {
+    //     head = new Node;
+    // } catch(std::bad_alloc &ba) {
+    //     ba.what();
+    //     return FAILURE;
+    // }   
+        if(isEmpty()) {
+            head = new Node;
+            head->data = addMe;
+            head->next = NULL;
+            tail = head;
+            return SUCCESS;
+        }
+        // temp pointer to head
+        Node* temp = new Node;
+        temp->data = addMe;
+        temp->next = head;
+        head = temp;
 
     return SUCCESS;
 }
 
 // removeback
-farmingdale::status farmingdale::LinkedList::removeBack(std::string &returnMe) {
+farmingdale::status farmingdale::LinkedList::removeBack() {
+    if(isEmpty()) {
+        std::cerr << "There was an error on line " << __LINE__ << std::endl;
+        return FAILURE;
+    }
 
-    // So the tail points to the back
+    // There is only one item in the linked list
+    if(NULL == head->next) {
+        delete head;
+        head = NULL;
+        tail = NULL;
+    }
 
-    // But this is a singly linked list but we need to preceeding node.
+    Node* nm1 = head;
+    while(nm1->next != tail) {
+        nm1 = nm1->next;
+    }
 
-    // Therefore we need to traverse the whole linked list.
-    // Node* nm1 = head;
-    // while(nm1->next != tail) {
-    //     nm1 = nm1->next;
-    // }
+    // We want to point the tail to the n-1 node
+    tail = nm1;
 
-    // // We want to point the tail to the n-1 node
-    // tail = nm1;
-
-    // // I want to make the n-1 node's next pointer to NULL
-    // nm1 = nm1->next;
-    // tail->next = NULL;
-    // delete nm1;
+    // I want to make the n-1 node's next pointer to NULL
+    nm1 = nm1->next;
+    tail->next = NULL;
+    delete nm1;
 
     return SUCCESS;
 }
 
 // addToBack
 farmingdale::status farmingdale::LinkedList::addToBack(std::string addMe) {
+    if(isEmpty()){
+        head = new Node;
+        head->data = addMe;
+        head->next = NULL;
+        tail = head;
+    }
     // I need a temp pointer
-    // Node* temp = tail;
+    Node* temp = new Node;
 
     // try {
-    //     temp->next = new Node(addMe);
+    //     temp->next = new Node;
     // } catch (std::bad_alloc &ba) {
     //     ba.what();
     //     return FAILURE;
     // }
-    // tail = temp->next;
+    temp->data = addMe;
+    temp->next = NULL;
+
+    tail->next = temp;
+    tail = temp;
+
     return SUCCESS;
 }
 
 // removeFront
-farmingdale::status farmingdale::LinkedList::removeFront(std::string &returnMe) {
+farmingdale::status farmingdale::LinkedList::removeFront() {
     // Check that the linked list is not empty ?
     if(isEmpty()) {
         return FAILURE;
     }
     Node* temp = head->next;
-    returnMe = head->data;
+    // returnMe = head->data; not needed, it returns status, call after getFront
     delete head;
     head = temp;
     return SUCCESS;
@@ -86,12 +110,20 @@ farmingdale::status farmingdale::LinkedList::removeFront(std::string &returnMe) 
 
 // search
 // farmingdale::Node * farmingdale::LinkedList::search()
-farmingdale::Node * farmingdale::LinkedList::search(std::string) {
+farmingdale::Node* farmingdale::LinkedList::search(std::string findMe) {
     if (isEmpty()){
-        return head;
+        std::cerr << "There was an error on line " << __LINE__ << std::endl;
+        return 0;
     };
     Node* temp = head;
-    return temp;
+    while(NULL != temp) {
+        // I have found it
+        if(findMe == temp->data) {
+            return temp;
+        }
+        temp = temp->next;
+    }
+    return 0;
 }
 
 // getByPosition
@@ -108,20 +140,20 @@ farmingdale::Node * farmingdale::LinkedList::getByPostion(int position) {
 
 // getFront
 farmingdale::status farmingdale::LinkedList::getFront(std::string &returnMe) {
-    // // If it is NONempty Return DATA
-    // if(!isEmpty()) {
-    //     returnMe = head->data;
-    //     return SUCCESS;
-    // }
+    // If it is NONempty Return DATA
+    if(!isEmpty()) {
+        returnMe = head->data;
+        return SUCCESS;
+    }
     return FAILURE;
 }
 
 // getBack
 farmingdale::status farmingdale::LinkedList::getBack(std::string &returnMe) {
-    // if(isEmpty()) {
-    //     returnMe = tail->data;
-    //     return SUCCESS;
-    // }
+    if(!isEmpty()) {
+        returnMe = tail->data;
+        return SUCCESS;
+    }
     return FAILURE;
 }
 
