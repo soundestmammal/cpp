64dca347f212797ef57061180efe120f2a1730e1
Robert Checco <robert.checco@gmail.com>
2019-10-03 23:56:09 -0400

    Front and Back Tests for Linked List (PASSING)

---
+++ DataStructures/P-Sets/A8/2019LinkedList/2019LinkedList/myTests.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <random>
+#include <list>
+#include <limits>
+#include <string>
+#include "linkedList.h"
+
+bool frontTest();
+bool backTest();
+
+int main() {
+    int x;
+    bool failing = false;
+    failing = frontTest();
+    if (failing) {
+        std::cerr << "Test failed on line " << __LINE__ << std::endl;
+        exit(1);
+    }
+    failing = backTest();
+    if (failing) {
+        std::cerr << "Test failed on line " << __LINE__ << std::endl;
+        exit(1);
+    }
+    std::cout << "Congrats your test passes." << std::endl;
+    std::cin >> x;
+    return 0;
+}
+
+// Tests addToFront and getFront against std::forward_list 
+bool frontTest() {
+    std::list<std::string> ForwardList;
+    farmingdale::LinkedList MyLinkedList;
+
+    int iterations = 100000;
+    for (int i = 0; i < iterations; i++)
+    {
+        ForwardList.push_front(std::to_string(iterations + i));    
+        MyLinkedList.addToFront(std::to_string(iterations + i)); 
+    }
+
+    while(!ForwardList.empty() && !MyLinkedList.isEmpty()) {
+        // std::cout << ForwardList.front() << std::endl;
+        // std::cout << MyLinkedList.getFront() << std::endl;
+        std::string s1, s2;
+        s1 = ForwardList.front();
+        MyLinkedList.getFront(s2);
+        std::cout << s1 << std::endl;
+        std::cout << s2 << std::endl;
+        // std::cout << s2.length() << std::endl;
+
+        if(s1 != s2) {
+            std::cerr << "There was an error on line " << __LINE__ << std::endl;
+            return true;
+        }
+        ForwardList.pop_front();
+        MyLinkedList.removeFront();
+    }
+
+    return false;
+};
+
+// Tests addToBack and getBack against std::list 
+bool backTest() {
+    std::list<std::string> ForwardList;
+    farmingdale::LinkedList MyLinkedList;
+
+    int iterations = 50000;
+    for (int i = 0; i < iterations; i++)
+    {
+        ForwardList.push_back(std::to_string(iterations + i));    
+        MyLinkedList.addToBack(std::to_string(iterations + i)); 
+    }
+
+    while(!ForwardList.empty() && !MyLinkedList.isEmpty()) {
+        // std::cout << ForwardList.front() << std::endl;
+        // std::cout << MyLinkedList.getFront() << std::endl;
+        std::string s1, s2;
+        s1 = ForwardList.back();
+        MyLinkedList.getBack(s2);
+        std::cout << s1 << std::endl;
+        std::cout << s2 << std::endl;
+        // std::cout << s2.length() << std::endl;
+
+        if(s1 != s2) {
+            std::cerr << "There was an error on line " << __LINE__ << std::endl;
+            return true;
+        }
+        ForwardList.pop_back();
+        MyLinkedList.removeBack();
+    }
+
+    return false;
+};
